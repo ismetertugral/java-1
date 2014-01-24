634a79df71807476ad186b96e429003d9ab81e32
oguzhantasci <oguzhantasci@outlook.com>
2014-01-24 16:02:29 +0200

    boolean

---
+++ boolean.java
@@ -0,0 +1,28 @@
+
+public class EXAMPLE3 {
+	
+	public static void main(String[] args)    //Boolean mantığını oturtmak için yazılmış olan koddur.
+	{
+		boolean b;
+		boolean a;
+
+		/*a=true;
+				System.out.println("A is "+a);
+		
+		b=false;
+				System.out.println("B is " +b);
+		
+		*/
+			b=false;
+			a=true;
+			
+			if(a)
+				System.out.println("sistem calisir");
+			if(b)
+				System.out.println("sistem calismaz");
+			
+			System.out.println("10>9 is " +(10>9));
+			
+	}
+
+}
