e1bf1c018898928e2f791c3ad8700b83cb52edbd
oguzhantasci <oguzhantasci@outlook.com>
2014-01-27 12:17:03 +0200

    hypotenuse

---
+++ hypotenuse.java
@@ -0,0 +1,12 @@
+public class hypotenuse {
+
+	public static void main(String[] args)
+	{
+		double a=3.0,b=4.0;
+		double c;
+		
+		c=Math.sqrt((a*a)+(b*b));
+		
+		System.out.println("Hypotenuse is "+c);
+	}
+}
