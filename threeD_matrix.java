d0ada8e6fa36ad889b278845779047973ce081b9
oguzhantasci <oguzhantasci@outlook.com>
2014-01-28 15:33:27 +0200

    threeD_matrix_3boyutlu_matrix

---
+++ threeD_matrix.java
@@ -0,0 +1,46 @@
+package boyutlu3matrix;
+
+public class _3boyutlumatrix {
+	
+	public static void main(String args[])
+	{
+		int threeD[][][]=new int[3][4][5];
+		int i,j,k;
+		for(i=0;i<3;i++)	//sayı atama işlemlerinin yapıldığı yerdir.
+			for(j=0;j<4;j++)
+				for(k=0;k<5;k++)
+					threeD[i][j][k]=i*j*k;
+		
+		for(i=0;i<3;i++){		//dizi boyutlarının belirlendiği kısımdır.
+			for(j=0;j<4;j++){
+				for(k=0;k<5;k++)
+					System.out.print(threeD[i][j][k]+" ");
+			System.out.println();
+		}
+		System.out.println();
+	}
+	}
+
+}
+
+/*
+ * Ekran Çıktısı/Screenshot
+ */
+ 0 0 0 0 0 
+0 0 0 0 0 
+0 0 0 0 0 
+0 0 0 0 0 
+
+0 0 0 0 0 
+0 1 2 3 4 
+0 2 4 6 8 
+0 3 6 9 12 
+
+0 0 0 0 0 
+0 2 4 6 8 
+0 4 8 12 16 
+0 6 12 18 24 
+
+
+ 
+*/
