43adef7f9e76c84e8498f1423910109e67193f14
oguzhantasci <oguzhantasci@outlook.com>
2014-01-28 15:43:21 +0200

    intro_st_string_yapısına_giris

---
+++ intro_string.java
@@ -0,0 +1,14 @@
+package stringGiris;
+
+public class stringGiris {
+	public static void main(String args[])
+	{
+		String str="This is a brief introduction trial.";
+				System.out.println(str);
+	}
+}
+/*
+Ekran Çıktısı/Screenshot
+
+This is a brief introduction trial.
+*/
