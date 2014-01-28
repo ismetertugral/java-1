5300d2ce2760ebf5ffaad2ebc307a9574676f2d2
oguzhantasci <oguzhantasci@outlook.com>
2014-01-28 14:06:20 +0200

    array_dizi_mantigi_3

---
+++ array_c.java
@@ -0,0 +1,20 @@
+package diziler_C;
+
+public class diziler_c {
+	public static void main(String args[])
+	{
+		double nums[]={10.1,11.2,12.3,13.4,14.5};
+		double result=0;
+		int i;
+		for(i=0;i<5;i++)
+		result=result+nums[i];
+		/*Döngü boyunca toplamı işlemi yapar.
+		 Loop through the process makes total.
+		 */
+		System.out.println("Avarage is "+result/5);
+		/*Ortalamayi ekrana yazdırır.
+		 Average on the screen to print.
+		 */
+	}
+
+}
