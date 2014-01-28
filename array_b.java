b87ea5176d9aa5c8b96a0ec0374a958c073350e2
oguzhantasci <oguzhantasci@outlook.com>
2014-01-28 13:51:32 +0200

    array_dizi mantigi_2

---
+++ array_b.java
@@ -0,0 +1,19 @@
+package diziler_b;
+
+public class diziler_b {
+	
+	public static void main(String args[]) 
+
+	{	
+	
+	    /*new ifadesinde bellekte yer açmaya gerek yoktur. 
+	    Bu şekilde yapıldığında otomayik olarak bellek tanımlanır.
+	    In this process "a" is not necessary to express.
+	    It is done automatically.*/
+	    
+		
+		int month_days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.println("August has "+month_days[7]+" days.");
+	}
+
+}
